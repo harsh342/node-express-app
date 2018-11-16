d6166885661052e7a4a5973ac8c726ec94d6d491
EC2 Default User <ec2-user@ip-172-31-11-120.eu-west-1.compute.internal>
2018-11-16 16:11:16 +0000

    new changes for page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var Express = require("express");
 var chalk = require("chalk");
 var debug = require("debug")("app");
-
+var morgan = require("morgan");
+var path = require("path");
 
 var express = new Express();
 
+express.use(morgan("tiny"));
+
 express.get("/",function(req,res){
-   res.send("This is an Express Library App!!") 
+   res.sendFile(path.join(__dirname,"views","Index.html")); 
 });
 
 express.listen(process.env.PORT,process.env.IP,function(){

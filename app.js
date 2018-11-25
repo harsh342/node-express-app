530097b44698c057aa4efc942683e5c2d90e95c4
EC2 Default User <ec2-user@ip-172-31-11-120.eu-west-1.compute.internal>
2018-11-25 22:19:23 +0000

    latest changes

--- app.js
+++ app.js
@@ -5,13 +5,31 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = new Express();
+const bookRouter = Express.Router();
 
 app.use(morgan('tiny'));
 
 app.use(Express.static(path.join(__dirname, '/public')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs')
+
+bookRouter.route('/').get((req, res) => {
+  res.send('Hello Books');
+});
+
+bookRouter.route('/single').get((req, res) => {
+  res.send('Hello Single Book');
+});
+
+
+app.use('/books', bookRouter);
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'Index.html'));
+  res.render('index', {
+    title: 'My Library',
+    nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }]
+  });
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
